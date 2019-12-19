3b0840bc5929999fdb9356fcea64724e1d632594
Muruges <muruges95@hotmail.com>
2019-12-19 20:54:23 +0800

    added styling for about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,52 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+// import { Link } from 'gatsby';
 
-import Layout, { NavbarPaddingContainer } from "../components/layout"
-import SEO from "../components/seo"
+import Layout, { NavbarPaddingContainer } from '../components/layout';
+import { PageHeaderFont, DescriptionFont, TitleFont } from '../components/textStyles';
+import SEO from '../components/seo';
+import styled from 'styled-components';
 
 const AboutPage = () => (
-  <Layout>
-    <NavbarPaddingContainer>
-      <SEO title="Page two" />
-      <h1>Hi from the about page</h1>
-      <p>Welcome to the about page</p>
-      <Link to="/">Go back to the homepage</Link>
-    </NavbarPaddingContainer>
-  </Layout>
-)
+	<Layout>
+		<NavbarPaddingContainer>
+			<SEO title="About Us" />
+			<AboutPageWrapper>Who we are.</AboutPageWrapper>
+			<VisionMissionWrapper>
+				<VisionMission>
+					<VisionMissionTitle>Our Vision.</VisionMissionTitle>
+					<VisionMissionDescription>
+						To be the rallying youth force that ushers in a space age in the region.
+					</VisionMissionDescription>
+				</VisionMission>
+				<VisionMission>
+					<VisionMissionTitle>Our Mission.</VisionMissionTitle>
+					<VisionMissionDescription>
+						To empower youth in Singapore & beyond to make an impace in space exploration.
+					</VisionMissionDescription>
+				</VisionMission>
+			</VisionMissionWrapper>
+		</NavbarPaddingContainer>
+	</Layout>
+);
 
-export default AboutPage
+const AboutPageWrapper = styled(PageHeaderFont)`
+  margin-bottom: 5rem;
+`;
+
+const VisionMissionWrapper = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-between;
+`;
+
+const VisionMission = styled.div`padding: 1rem;`;
+
+const VisionMissionTitle = styled(TitleFont)`
+  padding-bottom: 1rem;
+`;
+
+const VisionMissionDescription = styled(DescriptionFont)``;
+
+// const AboutSectionHeader = styled.
+
+export default AboutPage;
--- src/pages/events.js
+++ src/pages/events.js
@@ -1,18 +1,18 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link } from 'gatsby';
 
-import Layout, { NavbarPaddingContainer } from "../components/layout"
-import SEO from "../components/seo"
+import Layout, { NavbarPaddingContainer } from '../components/layout';
+import SEO from '../components/seo';
 
 const EventsPage = () => (
-  <Layout>
-    <NavbarPaddingContainer>
-      <SEO title="Page two" />
-      <h1>Hi from the events page</h1>
-      <p>Welcome to the events page</p>
-      <Link to="/">Go back to the homepage</Link>
-    </NavbarPaddingContainer>
-  </Layout>
-)
+	<Layout>
+		<NavbarPaddingContainer>
+			<SEO title="Events" />
+			<h1>Hi from the events page</h1>
+			<p>Welcome to the events page</p>
+			<Link to="/">Go back to the homepage</Link>
+		</NavbarPaddingContainer>
+	</Layout>
+);
 
-export default EventsPage
+export default EventsPage;
