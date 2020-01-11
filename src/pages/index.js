55431b656e298e57cc18c73ceba98d0934f6a590
Muruges <muruges95@hotmail.com>
2020-01-11 20:05:05 +0800

    fixed z-index of bg images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,110 +1,126 @@
-import React, { useState, useEffect } from 'react';
-import Layout, { NavbarPaddingContainer } from '../components/layout';
-import { LandingTitleFont, DescriptionFont } from '../components/textStyles';
-import BackgroundImage from 'gatsby-background-image';
-import SEO from '../components/seo';
-import { useStaticQuery, graphql } from 'gatsby';
-import styled from 'styled-components';
-import StyledButton from '../components/button';
+import React, { useState, useEffect } from "react"
+import Layout, { NavbarPaddingContainer } from "../components/layout"
+import { LandingTitleFont, DescriptionFont } from "../components/textStyles"
+import BackgroundImage from "gatsby-background-image"
+import SEO from "../components/seo"
+import { useStaticQuery, graphql } from "gatsby"
+import styled from "styled-components"
+import StyledButton from "../components/button"
 
 const IndexPage = () => {
-	const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0)
 
-	useEffect(() => {
-		const intervalHandle = setInterval(() => setCurrentIndex((prevIndex) => (prevIndex + 1) % 2), 10000);
-		return () => {
-			clearInterval(intervalHandle);
-		};
-	}, []);
+  useEffect(() => {
+    const intervalHandle = setInterval(
+      () => setCurrentIndex(prevIndex => (prevIndex + 1) % 2),
+      10000
+    )
+    return () => {
+      clearInterval(intervalHandle)
+    }
+  }, [])
 
-	const { darpa, newOppy } = useStaticQuery(
-		graphql`
-			query {
-				darpa: file(relativePath: { eq: "darpa.png" }) {
-					childImageSharp {
-						fluid(quality: 100, maxWidth: 1440) {
-							...GatsbyImageSharpFluid_withWebp
-						}
-					}
-				}
-				newOppy: file(relativePath: { eq: "new_oppy.png" }) {
-					childImageSharp {
-						fluid(quality: 100, maxWidth: 1440) {
-							...GatsbyImageSharpFluid_withWebp
-						}
-					}
-				}
-			}
-		`
-	);
+  const { darpa, newOppy } = useStaticQuery(
+    graphql`
+      query {
+        darpa: file(relativePath: { eq: "darpa.png" }) {
+          childImageSharp {
+            fluid(quality: 100, maxWidth: 1440) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+        newOppy: file(relativePath: { eq: "new_oppy.png" }) {
+          childImageSharp {
+            fluid(quality: 100, maxWidth: 1440) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    `
+  )
 
-	return (
-		<Layout>
-			{[0, 1].map((projectIndex) => (
-				<BgImgContainer key={projectIndex} projectIndex={projectIndex}
-					currentIndex={currentIndex}>
-					<StyledBackgroundImage
-						Tag="section"
-						fluid={projectIndex === 0 ? newOppy.childImageSharp.fluid : darpa.childImageSharp.fluid}
-						preserveStackingContext={true}
-						style={{
-							position: 'fixed'
-						}}
-					>
-						<StyledNavBarPadding>
-							<SEO title="Home" />
-							<div>
-								<ProjectTitleWrapper>
-									{projectIndex === 0 ? 'New Opportunity' : 'DARPA Subterranean Challenge'}
-								</ProjectTitleWrapper>
-								<ProjectDecriptionWrapper>
-									{projectIndex === 0 ? (
-										'We are building an autonomous exploration rover from scratch.'
-									) : (
-											'We are helping to revolutionize how we operate in the underground domain.'
-										)}
-								</ProjectDecriptionWrapper>
-								<ProjectDetailsButton onClick={() => window.location = (projectIndex === 0 ? '/new-oppy' : 'darpa-subt')} buttonText="Learn More" />
-							</div>
-						</StyledNavBarPadding>
-					</StyledBackgroundImage>
-				</BgImgContainer>
-			))}
-		</Layout>
-	);
-};
+  return (
+    <Layout>
+      {[0, 1].map(projectIndex => (
+        <BgImgContainer
+          key={projectIndex}
+          selected={projectIndex === currentIndex}
+        >
+          <StyledBackgroundImage
+            Tag="section"
+            fluid={
+              projectIndex === 0
+                ? newOppy.childImageSharp.fluid
+                : darpa.childImageSharp.fluid
+            }
+            preserveStackingContext={true}
+            style={{
+              position: "fixed",
+            }}
+          >
+            <StyledNavBarPadding>
+              <SEO title="Home" />
+              <div>
+                <ProjectTitleWrapper>
+                  {projectIndex === 0
+                    ? "New Opportunity"
+                    : "DARPA Subterranean Challenge"}
+                </ProjectTitleWrapper>
+                <ProjectDecriptionWrapper>
+                  {projectIndex === 0
+                    ? "We are building an autonomous exploration rover from scratch."
+                    : "We are helping to revolutionize how we operate in the underground domain."}
+                </ProjectDecriptionWrapper>
+                <ProjectDetailsButton
+                  onClick={() =>
+                    (window.location =
+                      projectIndex === 0 ? "/new-oppy" : "darpa-subt")
+                  }
+                  buttonText="Learn More"
+                />
+              </div>
+            </StyledNavBarPadding>
+          </StyledBackgroundImage>
+        </BgImgContainer>
+      ))}
+    </Layout>
+  )
+}
 
 const StyledNavBarPadding = styled(NavbarPaddingContainer)`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	height: 100vh;
-`;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100vh;
+`
 
 const ProjectDetailsButton = styled(StyledButton)`
-	background-color: #5557F6;
-	border: 0;
-	margin-top: 3rem;
-	padding: 0.75rem 2.5rem; 
-`;
+  background-color: #5557f6;
+  border: 0;
+  margin-top: 3rem;
+  padding: 0.75rem 2.5rem;
+`
 
 const ProjectTitleWrapper = styled(LandingTitleFont)`
   margin-bottom: 0.75rem;
-`;
+`
 
 const ProjectDecriptionWrapper = styled(DescriptionFont)`
   font-size: 1.875rem;
-`;
+`
 
 const StyledBackgroundImage = styled(BackgroundImage)`
   width: 100%;
-	height: 100vh;
-`;
-
-const BgImgContainer = styled.div`
-	opacity: ${props => (props.projectIndex === props.currentIndex) ? 1 : 0};
-	transition: opacity 2s ease-in;
-	position: fixed;
+  height: 100vh;
 `
 
-export default IndexPage;
+const BgImgContainer = styled.div`
+  opacity: ${props => (props.selected ? 1 : 0)};
+  z-index: ${props => (props.selected ? 2 : 1)};
+  transition: opacity 2s ease-in;
+  position: fixed;
+`
+
+export default IndexPage
