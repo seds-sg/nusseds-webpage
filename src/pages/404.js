27a243f62b34e3ecbea06e6767f4f44f5ab3dc1c
Muruges <muruges95@hotmail.com>
2020-01-16 11:41:43 +0800

    added error message with styling

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,25 @@
 import React from "react"
-
-import { NavbarPaddingContainer } from "../components/layout"
+import { PageHeaderFont, DescriptionFont } from '../components/textStyles';
+import Layout, { NavbarPaddingContainer } from "../components/layout"
 import SEO from "../components/seo"
+import styled from 'styled-components';
 
 const NotFoundPage = () => (
-  <NavbarPaddingContainer>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </NavbarPaddingContainer>
+  <Layout>
+    <NavbarPaddingContainer>
+      <SEO title="404: Not found" />
+      <NotFoundPageHeaderWrapper>NOT FOUND</NotFoundPageHeaderWrapper>
+      <NotFoundPageDescription>You just hit a route that doesn&#39;t exist... the sadness.</NotFoundPageDescription>
+    </NavbarPaddingContainer>
+  </Layout>
 )
+
+const NotFoundPageHeaderWrapper = styled(PageHeaderFont)`
+  margin-bottom: 3rem;
+`;
+
+const NotFoundPageDescription = styled(DescriptionFont)`
+  padding-bottom: 4.5rem;
+`;
 
 export default NotFoundPage
