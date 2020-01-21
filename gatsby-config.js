b4021f2daecb6e44cf9cd22ab1950c1ed52d8caa
Muruges <muruges95@hotmail.com>
2020-01-21 11:29:29 +0800

    fixed fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,52 +1,52 @@
 module.exports = {
-  siteMetadata: {
-    title: `NUS SEDS Site`,
-    description: `Webpage for anything related to the NUS chapter of the Students for Exploration and Development of Space`,
-    author: `@muruges95`,
-  },
-  plugins: [
-    `gatsby-transformer-remark`,
-    `gatsby-plugin-styled-components`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `content`,
-        path: `${__dirname}/src/content`,
-      },
-    },
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `nusseds-webpage`,
-        short_name: `nusseds`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    {
-      resolve: `gatsby-plugin-web-font-loader`,
-      options: {
-        custom: {
-          families: [`Graphik, Graebenbach Mono, Brown`],
-          urls: [`/fonts.css`],
-        },
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
-}
+	siteMetadata: {
+		title: `NUS SEDS Site`,
+		description: `Webpage for anything related to the NUS chapter of the Students for Exploration and Development of Space`,
+		author: `@muruges95`
+	},
+	plugins: [
+		`gatsby-transformer-remark`,
+		`gatsby-plugin-styled-components`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `content`,
+				path: `${__dirname}/src/content`
+			}
+		},
+		`gatsby-plugin-react-helmet`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/images`
+			}
+		},
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `nusseds-webpage`,
+				short_name: `nusseds`,
+				start_url: `/`,
+				background_color: `#663399`,
+				theme_color: `#663399`,
+				display: `minimal-ui`,
+				icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+			}
+		},
+		{
+			resolve: `gatsby-plugin-web-font-loader`,
+			options: {
+				custom: {
+					families: [ `Graphik, Brown, Graebenbach Mono` ],
+					urls: [ `/fonts/fonts.css` ]
+				}
+			}
+		}
+		// this (optional) plugin enables Progressive Web App + Offline functionality
+		// To learn more, visit: https://gatsby.dev/offline
+		// `gatsby-plugin-offline`,
+	]
+};
--- src/components/textStyles.js
+++ src/components/textStyles.js
@@ -1,9 +1,9 @@
-import styled from "styled-components"
+import styled from 'styled-components';
 
 const BaseFont = styled.div`
-  color: #f8f3e9;
-  /* SET FONT FAMILY HERE */
-`
+	color: #f8f3e9;
+	/* SET FONT FAMILY HERE */
+`;
 
 export const LandingTitleFont = styled(BaseFont)`
   font-weight: 700;
@@ -14,7 +14,7 @@ export const LandingTitleFont = styled(BaseFont)`
   @media only screen and (max-width: 600px) {
     font-size: 2.5rem;
   }
-`
+`;
 
 export const PageHeaderFont = styled(BaseFont)`
   font-weight: 700;
@@ -24,17 +24,17 @@ export const PageHeaderFont = styled(BaseFont)`
   @media only screen and (max-width: 600px) {
     font-size: 2.5rem;
   }
-`
+`;
 
 export const TitleFont = styled(BaseFont)`
   font-weight: 700;
   font-size: 3rem;
   font-family: Brown;
-  letter-spacing: -0.04em;\
+  letter-spacing: -0.04em;
   @media only screen and (max-width: 600px) {
     font-size: 2rem;
   }
-`
+`;
 
 export const DescriptionFont = styled(BaseFont)`
   font-size: 1.2rem;
@@ -45,26 +45,26 @@ export const DescriptionFont = styled(BaseFont)`
   @media only screen and (max-width: 600px) {
     font-size: 1rem;
   }
-`
+`;
 
 export const SmallFont = styled(BaseFont)`
   font-size: 0.875rem;
   font-weight: 700;
   font-family: Brown;
-`
+`;
 
 export const ButtonFont = styled(BaseFont)`
   font-family: Brown;
   font-weight: 700;
   font-size: 1rem;
   letter-spacing: 0.02em;
-`
+`;
 
 export const NavBarFont = styled(BaseFont)`
   font-family: Brown;
   font-weight: 400;
   font-size: 1rem;
-`
+`;
 
 /**
  * If it is found that we are using many diferent font sizes and weights use the props way to pass in the weights and sizes: https://www.styled-components.com/docs/basics#adapting-based-on-props
