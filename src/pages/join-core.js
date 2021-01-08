902a2cb7313e693f0560ffb01bcba66f2fceb7d5
Ian Wu <iwle@u.nus.edu>
2021-01-08 14:17:35 +0800

    Add new pages

---
+++ src/pages/join-core.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { PageHeaderFont, DescriptionFont } from "../components/textStyles"
+import Layout, { NavbarPaddingContainer } from "../components/layout"
+import SEO from "../components/seo"
+import styled from "styled-components"
+import { useStaticQuery, graphql } from "gatsby"
+import { ImageWrapper, StyledImageWithTitle } from "../components/leadership"
+
+const JoinCore = () => {
+	componentDidMount () {
+		window.location.replace("http://www.google.com")
+	}
+
+	return ( )
+}
+
+const ProjectsPageHeaderWrapper = styled(PageHeaderFont)`
+  margin-bottom: 3rem;
+`
+
+const ProjectsPageDescription = styled(DescriptionFont)`
+  padding-bottom: 4.5rem;
+`
+
+export default ProjectsPage
